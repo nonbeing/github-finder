a4ffee8ba235bd28b6596d52096f01e47109636d
nonbeing <anattaspace@gmail.com>
2019-12-18 19:13:24 +0530

    11: simple state, destructure object

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor() {
-    super()
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo'
-    }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo'
   }
+
   render() {
+    const { avatar_url, login, html_url } = this.state
+
     return (
       <div className='card text-center'>
-        <img
-          src={this.state.avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        ></img>
-        <h3>{this.state.login}</h3>
+        <img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }}></img>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
             More
           </a>
         </div>
