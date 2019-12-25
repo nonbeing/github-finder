5a0e48927a135147b5bd3feac0080daf80da163e
nonbeing <anattaspace@gmail.com>
2019-12-25 14:44:58 +0530

    15: spinner component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 
 import './App.css'
 
 class App extends Component {
+  GitHubUserURL = 'https://api.github.com/users'
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const res = await axios.get(this.GitHubUserURL)
+    console.log('res.data', res.data)
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     )
